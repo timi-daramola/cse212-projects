613463fc8f585ed31f87604e6e0cfe3499080ba0
Timilehin Daramola <timi.homezone111@gmail.com>
2025-05-23 18:18:18 -0400

    Queues and tests

--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -3,9 +3,8 @@
     private List<PriorityItem> _queue = new();
 
     /// <summary>
-    /// Add a new value to the queue with an associated priority.  The
-    /// node is always added to the back of the queue regardless of 
-    /// the priority.
+    /// Add a new value to the queue with an associated priority.  
+    /// The node is always added to the back of the queue regardless of the priority.
     /// </summary>
     /// <param name="value">The value</param>
     /// <param name="priority">The priority</param>
@@ -15,6 +14,11 @@
         _queue.Add(newNode);
     }
 
+    /// <summary>
+    /// Remove and return the item with the highest priority.
+    /// If there are multiple items with the same priority, the first one (FIFO) is removed.
+    /// </summary>
+    /// <returns>The value of the dequeued item</returns>
     public string Dequeue()
     {
         if (_queue.Count == 0) // Verify the queue is not empty
@@ -22,16 +26,19 @@
             throw new InvalidOperationException("The queue is empty.");
         }
 
-        // Find the index of the item with the highest priority to remove
+        // Find the index of the item with the highest priority
         var highPriorityIndex = 0;
-        for (int index = 1; index < _queue.Count - 1; index++)
+        for (int index = 1; index < _queue.Count; index++)
         {
-            if (_queue[index].Priority >= _queue[highPriorityIndex].Priority)
+            if (_queue[index].Priority > _queue[highPriorityIndex].Priority)
+            {
                 highPriorityIndex = index;
+            }
         }
 
         // Remove and return the item with the highest priority
         var value = _queue[highPriorityIndex].Value;
+        _queue.RemoveAt(highPriorityIndex); // Remove the item from the queue
         return value;
     }
 
--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -1,20 +1,14 @@
-/// <summary>
-/// This queue is circular.  When people are added via AddPerson, then they are added to the 
-/// back of the queue (per FIFO rules).  When GetNextPerson is called, the next person
-/// in the queue is saved to be returned and then they are placed back into the back of the queue.  Thus,
-/// each person stays in the queue and is given turns.  When a person is added to the queue, 
-/// a turns parameter is provided to identify how many turns they will be given.  If the turns is 0 or
-/// less than they will stay in the queue forever.  If a person is out of turns then they will 
-/// not be added back into the queue.
-/// </summary>
+using System;
+using System.Collections.Generic;
+
 public class TakingTurnsQueue
 {
-    private readonly PersonQueue _people = new();
+    private readonly Queue<Person> _people = new();
 
-    public int Length => _people.Length;
+    public int Length => _people.Count;
 
     /// <summary>
-    /// Add new people to the queue with a name and number of turns
+    /// Add a new person to the queue with a name and number of turns.
     /// </summary>
     /// <param name="name">Name of the person</param>
     /// <param name="turns">Number of turns remaining</param>
@@ -27,31 +21,37 @@ public class TakingTurnsQueue
     /// <summary>
     /// Get the next person in the queue and return them. The person should
     /// go to the back of the queue again unless the turns variable shows that they 
-    /// have no more turns left.  Note that a turns value of 0 or less means the 
-    /// person has an infinite number of turns.  An error exception is thrown 
+    /// have no more turns left. Note that a turns value of 0 or less means the 
+    /// person has an infinite number of turns. An error exception is thrown 
     /// if the queue is empty.
     /// </summary>
     public Person GetNextPerson()
     {
-        if (_people.IsEmpty())
+        if (_people.Count == 0)
         {
             throw new InvalidOperationException("No one in the queue.");
         }
-        else
-        {
-            Person person = _people.Dequeue();
-            if (person.Turns > 1)
-            {
-                person.Turns -= 1;
-                _people.Enqueue(person);
-            }
 
-            return person;
+        var person = _people.Dequeue();
+
+        // Handle infinite turns (Turns <= 0)
+        if (person.Turns <= 0)
+        {
+            _people.Enqueue(person);
         }
+        // Handle finite turns (Turns > 1)
+        else if (person.Turns > 1)
+        {
+            person.Turns -= 1;
+            _people.Enqueue(person);
+        }
+        // If Turns == 1, do not re-enqueue the person (they are out of turns)
+
+        return person;
     }
 
     public override string ToString()
     {
-        return _people.ToString();
+        return string.Join(", ", _people);
     }
 }
